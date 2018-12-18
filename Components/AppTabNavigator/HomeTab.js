e6531d69fc9a0604a655e50e60074b01939fff23
Gaurav Virmani <virmanigaurav97@gmail.com>
2018-12-19 00:16:56 +0530

    Added stories

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Container, Content, Icon } from "native-base";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { Container, Content, Icon, Thumbnail } from "native-base";
 import CardComponent from "../CardComponent";
 
 class HomeTab extends Component {
@@ -13,6 +13,132 @@ class HomeTab extends Component {
     return (
       <Container>
         <Content>
+          <View style={{ height: 100 }}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingHorizontal: 7
+              }}
+            >
+              <Text>Stories</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between"
+                }}
+              >
+                <Icon
+                  name="md-play"
+                  style={{ fontSize: 14, fontWeight: "bold" }}
+                />
+                <Text> WatchAll</Text>
+              </View>
+            </View>
+            <View style={{ flex: 3 }}>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                  alignItems: "center",
+                  paddingStart: 5,
+                  paddingEnd: 5
+                }}
+              >
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/1.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/2.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/3.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/4.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/5.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/6.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/7.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/8.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/9.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/10.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 3,
+                    marginHorizontal: 5
+                  }}
+                  source={require("../../assets/stories/11.jpg")}
+                />
+              </ScrollView>
+            </View>
+          </View>
           <CardComponent
             likes="101"
             profilePic="https://picsum.photos/458/354/?image=122"
