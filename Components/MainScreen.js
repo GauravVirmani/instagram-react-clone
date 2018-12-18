86b29294eb8b1610eb2afa2f420fd67ec2c47f65
Gaurav Virmani <virmanigaurav97@gmail.com>
2018-12-18 14:56:22 +0530

    Implemented nav tabs with icons

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,22 +1,63 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Icon } from "native-base";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+
+import HomeTab from "./AppTabNavigator/HomeTab";
+import LikesTab from "./AppTabNavigator/LikesTab";
+import SearchTab from "./AppTabNavigator/SearchTab";
+import AddMediaTab from "./AppTabNavigator/AddMediaTab";
+import ProfileTab from "./AppTabNavigator/ProfileTab";
 
 class MainScreen extends Component {
+  static navigationOptions = {
+    headerTitleStyle: {
+      textAlign: "center",
+      flex: 1
+    },
+    headerLeft: <Icon style={{ paddingLeft: 10 }} name="ios-camera" />,
+    title: "Instagram",
+    headerRight: <Icon style={{ paddingRight: 10 }} name="ios-send" />
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>MainScreen</Text>
-      </View>
+      //   <View style={styles.container}>
+      //     <Text>MainScreen</Text>
+      //   </View>
+      //   <AppTabNavigator />
+      <AppTabContainer />
     );
   }
 }
 
-export default MainScreen;
+const AppTabNavigator = createBottomTabNavigator({
+  Home: {
+    screen: HomeTab
+  },
+  Search: {
+    screen: SearchTab
+  },
+  AddMedia: {
+    screen: AddMediaTab
+  },
+  Likes: {
+    screen: LikesTab
+  },
+  Profile: {
+    screen: ProfileTab
+  }
+});
+
+const AppTabContainer = createAppContainer(AppTabNavigator);
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#fff",
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
   }
 });
+
+export default MainScreen;
